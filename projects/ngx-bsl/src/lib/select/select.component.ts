9dc397aee2e0e0f1478ec00c9d60ec0f3a9f8da1
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2025-12-23 11:37:21 +0100

    refactor(select): switch to external option definition

--- projects/ngx-bsl/src/lib/select/select.component.ts
+++ projects/ngx-bsl/src/lib/select/select.component.ts
@@ -1,18 +1,18 @@
 import {ChangeDetectionStrategy,
     Component,
     computed,
+    DestroyRef,
     forwardRef,
+    inject,
     input,
     model,
     signal,
-    viewChild,
-    ViewEncapsulation} from '@angular/core';
+    ViewEncapsulation, OnInit} from '@angular/core';
 import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
 import {CdkConnectedOverlay, CdkOverlayOrigin} from '@angular/cdk/overlay';
 import {IconChevronDownComponent} from '../icons/icon-chevron-down.component';
-import {ListBoxComponent} from '../list-box/list-box.component';
-import {ListBoxOptionComponent} from '../list-box/list-box-option/list-box-option.component';
-import {ListBoxOptionValueConverterPipe} from '../list-box/list-box-option/list-box-option-value-converter.pipe';
+import {ListBoxDirective} from '../list-box/list-box.directive';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 
 @Component({
     selector: 'ngx-bsl-select',
@@ -20,9 +20,6 @@ import {ListBoxOptionValueConverterPipe} from '../list-box/list-box-option/list-
         CdkOverlayOrigin,
         CdkConnectedOverlay,
         IconChevronDownComponent,
-        ListBoxComponent,
-        ListBoxOptionComponent,
-        ListBoxOptionValueConverterPipe,
     ],
     templateUrl: './select.component.html',
     styleUrl: './select.component.scss',
@@ -35,35 +32,69 @@ import {ListBoxOptionValueConverterPipe} from '../list-box/list-box-option/list-
     ],
     changeDetection: ChangeDetectionStrategy.OnPush,
     encapsulation: ViewEncapsulation.None,
+    hostDirectives: [
+        {
+            directive: ListBoxDirective,
+            inputs: ['listBoxId', 'listBoxAriaLabel', 'listBoxAriaLabelledby', 'optionValueEquality'],
+        },
+    ],
 })
-export class SelectComponent<TOption> implements ControlValueAccessor {
+export class SelectComponent<TOption, TValue> implements ControlValueAccessor, OnInit {
+    protected listBox = inject(ListBoxDirective<TOption>);
+    private destroyRef = inject(DestroyRef);
+
     id = input.required<string>();
-    options = input.required<TOption[]>();
-    bindLabel = input<string>();
     placeholder = input<string>('');
     ariaLabel = input<string>();
     ariaLabelledBy = input<string>();
-    comparisonField = input<keyof TOption>();
     iconMode = input(false);
     customIcon = input(false);
     dropdownWidth = input<string>();
+    optionValueParse = input<(option: TOption) => TValue>((option) => option as unknown as TValue);
+    displayLabel = input<string>();
 
-    private listBox = viewChild(ListBoxComponent);
-
-    onChange = (_value: TOption) => {};
+    onChange = (_value: TValue) => {};
     onTouch = () => {};
-    value = model<TOption>();
+
+    value = model<TValue>();
 
     protected open = signal(false);
-    protected ariaActiveDescendant = computed<string | null>(() => this.listBox()?.ariaActiveDescendant() ?? null);
-    protected initialFocusedOptionIndex = signal<number | null>(null);
+    protected ariaActiveDescendant = computed<string | null>(() => this.listBox.ariaActiveDescendant() ?? null);
+    protected displayValue = computed(() => {
+        const displayLabel = this.displayLabel();
+        const value = this.value();
+
+        if (displayLabel && value && typeof value === 'object' && displayLabel in value) {
+            return value[displayLabel as keyof TValue];
+        } else {
+            return value;
+        }
+    });
+
+    ngOnInit(): void {
+        this.listBox.hasAriaSelected = true;
+        this.subscribeSelectOption();
+    }
+
+    private subscribeSelectOption(): void {
+        this.listBox.selectOption.pipe(
+            takeUntilDestroyed(this.destroyRef),
+        ).subscribe(option => {
+            if (option) {
+                this.value.set(this.optionValueParse()(option));
+                this.onChange(this.value() as TValue);
+            }
+            this.hideListBox();
+        });
+    }
 
     protected showListBox(): void {
         this.open.set(true);
+        this.listBox.initSelectedOption(this.value());
     }
 
     protected hideListBox(): void {
-        this.initialFocusedOptionIndex.set(null);
+        this.listBox.clearVisualFocus();
         this.open.set(false);
     }
 
@@ -71,14 +102,14 @@ export class SelectComponent<TOption> implements ControlValueAccessor {
         if (this.open()) {
             this.hideListBox();
         } else {
-            this.showListBox();
+            if (this.listBox.listBoxOptions().length) {
+                this.showListBox();
+            }
         }
     }
 
-    protected onSelectOption(value: TOption): void {
-        this.value.set(value);
-        this.onChange(value);
-        this.hideListBox();
+    protected onListBoxClick(event: MouseEvent): void {
+        this.listBox.onClick(event);
     }
 
     protected onKeydown(event: KeyboardEvent) {
@@ -87,16 +118,19 @@ export class SelectComponent<TOption> implements ControlValueAccessor {
         if (!this.open()) {
             this.showListBox();
             if (event.code === 'ArrowUp') {
-                this.initialFocusedOptionIndex.set(this.options().length - 1);
+                this.listBox.setVisualFocus(this.listBox.listBoxOptions().length - 1);
+                this.showListBox();
             } else if (event.code === 'ArrowDown') {
-                this.initialFocusedOptionIndex.set(0);
+                if (!this.listBox.listBoxOptions().length) return;
+                this.listBox.setVisualFocus(0);
+                this.showListBox();
             }
         } else {
-            this.listBox()?.onKeydown(event);
+            this.listBox.onKeydown(event);
         }
     }
 
-    registerOnChange(onChange: (value: TOption) => void): void {
+    registerOnChange(onChange: (value: TValue) => void): void {
         this.onChange = onChange;
     }
 
@@ -104,7 +138,7 @@ export class SelectComponent<TOption> implements ControlValueAccessor {
         this.onTouch = onTouch;
     }
 
-    writeValue(value: TOption): void {
+    writeValue(value: TValue): void {
         this.value.set(value);
     }
 }
